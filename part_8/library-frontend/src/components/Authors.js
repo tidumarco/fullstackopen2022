d878e4e44563f2d5b6ff9a4942cd860cc4a4037e
Marco Tidu <tidumarco@gmail.com>
2023-02-21 07:41:47 +0200

    Exercise 8.8 done

---
+++ part_8/library-frontend/src/components/Authors.js
@@ -0,0 +1,50 @@
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_AUTHORS = gql`
+  query AllAuthors {
+    allAuthors {
+      name
+      born
+      id
+      bookCount
+    }
+  }
+`;
+
+const Authors = (props) => {
+  const result = useQuery(ALL_AUTHORS);
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
+  if (!props.show) {
+    return null;
+  }
+  const authors = result.data.allAuthors;
+  console.log(authors)
+
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {authors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Authors;
