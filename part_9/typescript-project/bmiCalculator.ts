22d733dab2ed9dd0d1f6646e576e1e9b9ff5382e
Marco Tidu <tidumarco@gmail.com>
2023-02-24 09:24:55 +0200

    Exercise 9.1 done

---
+++ part_9/typescript-project/bmiCalculator.ts
@@ -0,0 +1,44 @@
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / (height / 100) ** 2;
+  if (bmi < 15) {
+    return "Very severely underweight";
+  } else if (bmi < 16) {
+    return "Severely underweight";
+  } else if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi < 25) {
+    return "Normal (healthy weight)";
+  } else if (bmi < 30) {
+    return "Overweight";
+  } else if (bmi < 35) {
+    return "Obese Class I (Moderately obese)";
+  } else if (bmi < 40) {
+    return "Obese Class II (Severely obese)";
+  } else {
+    return "Obese Class III (Very severely obese)";
+  }
+};
+
+const parseArguments = (args: string[]): { height: number; weight: number } => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided values were not numbers!");
+  }
+};
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = "Something bad happened.";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
