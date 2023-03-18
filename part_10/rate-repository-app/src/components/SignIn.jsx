1df67bdd63316a566871ccd5495d72b860225be5
Marco Tidu <tidumarco@gmail.com>
2023-03-18 22:29:38 +0200

    Exercise 10.8 done

--- part_10/rate-repository-app/src/components/SignIn.jsx
+++ part_10/rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,76 @@
+import { Formik } from "formik";
+import { StyleSheet, Pressable, TextInput, View } from "react-native";
 import Text from "./Text";
 
+const onSubmit = (values) => {
+  console.log(values);
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 15,
+  },
+  inputContainer: {
+    marginBottom: 10,
+  },
+  input: {
+    backgroundColor: "#fff",
+    height: 40,
+    padding: 10,
+    borderRadius: 5,
+  },
+  button: {
+    backgroundColor: "#0366d6",
+    borderRadius: 5,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
+
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={onSubmit}
+      >
+        {({ handleChange, handleBlur, handleSubmit, values }) => (
+          <View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                onChangeText={handleChange("username")}
+                onBlur={handleBlur("username")}
+                value={values.username}
+                placeholder="Username"
+                style={styles.input}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+                placeholder="Password"
+                style={styles.input}
+                secureTextEntry
+              />
+            </View>
+            <Pressable onPress={handleSubmit} style={styles.button}>
+              <Text style={styles.buttonText}>Sign in</Text>
+            </Pressable>
+          </View>
+        )}
+      </Formik>
+    </View>
+  );
 };
 
 export default SignIn;
